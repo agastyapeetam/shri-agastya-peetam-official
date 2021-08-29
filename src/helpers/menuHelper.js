0568627c714a9d6bf45a63878cc2285519179311
ksuryas <surya.kasibhatla@gmail.com>
2021-08-29 15:09:45 +0530

    Final version as of Aug 29, 2021

---
+++ src/helpers/menuHelper.js
@@ -0,0 +1,24 @@
+let navItems = [
+    {
+        link: "/",
+        name: ''
+    },
+    {
+        link: "/",
+        name: 'About Us'
+    },
+    {
+        link: "/about-swamiji/",
+        name: 'Shri Swami Sugunananda Gayathri'
+    },
+    {
+        link: "/about-gayathri-maha-purashcharana/",
+        name: 'Gayathri Maha Purascharana'
+    },
+    {
+        link: "/about-deergha-sathra-yagam/",
+        name: 'Deergha Sathra Yagam'
+    }
+];
+
+export default navItems;
